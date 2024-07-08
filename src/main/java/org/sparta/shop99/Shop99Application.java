9096a67b9e84fe65d75b3a107b9c55c9f8dd0f9e
yeobi-it <mrd1994it@gmail.com>
2024-07-08 10:23:22 +0900

    Init commit

---
+++ src/main/java/org/sparta/shop99/Shop99Application.java
@@ -0,0 +1,13 @@
+package org.sparta.shop99;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shop99Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Shop99Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/sparta/shop99/Shop99ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sparta.shop99;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shop99ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
